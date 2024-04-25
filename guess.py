777d78aefadecbeb17e6af49e44c3d4c47901495
Ibrakhim Yuldashev <ibayuldashev@gmail.com>
2024-04-25 23:59:56 +0500

    repeat until player wins

--- guess.py
+++ guess.py
@@ -3,8 +3,6 @@ import random
 n = random.randint(0,10)
 guess = int(input("guess a number between 0 and 10"))
 
-if n == guess:
-    print("Correct")
-else :
-    print("Incorrect")
-
+While True:
+    print("Correct :)")
+    break
